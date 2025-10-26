61ff1805ff524d2ac7e310cc797b00f8208c99fc
James Cox-Morton <th3james@hey.com>
2025-10-26 11:27:44 +0000

    Start on parser

--- src/calculate.zig
+++ src/calculate.zig
@@ -18,14 +18,70 @@ fn advance_past_whitespace(json_reader: *std.Io.Reader) !void {
     }
 }
 
-fn advance_to_first_pair(json_reader: *std.Io.Reader) !void {
+test "advance_past_whitespace with spaces and tabs" {
+    const test_input = "   \t\n\r{";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [16]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try advance_past_whitespace(&limited.interface);
+
+    const next_byte = try limited.interface.peek(1);
+    try std.testing.expect(next_byte.len > 0);
+    try std.testing.expectEqual('{', next_byte[0]);
+}
+
+test "advance_past_whitespace with no whitespace" {
+    const test_input = "{\"test\": 123}";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [16]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try advance_past_whitespace(&limited.interface);
+
+    const next_byte = try limited.interface.peek(1);
+    try std.testing.expect(next_byte.len > 0);
+    try std.testing.expectEqual('{', next_byte[0]);
+}
+
+test "advance_past_whitespace with empty stream" {
+    const test_input = "";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [16]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try advance_past_whitespace(&limited.interface);
+
+    const next_byte = limited.interface.peek(1) catch |err| switch (err) {
+        error.EndOfStream => return, // Expected for empty stream
+        else => return err,
+    };
+    try std.testing.expectEqual(0, next_byte.len);
+}
+
+test "advance_past_whitespace with only whitespace" {
+    const test_input = "   \t\n\r   ";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [64]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try advance_past_whitespace(&limited.interface);
+
+    const next_byte = limited.interface.peek(1) catch |err| switch (err) {
+        error.EndOfStream => return, // Expected for empty stream
+        else => return err,
+    };
+    try std.testing.expectEqual(0, next_byte.len);
+}
+
+fn advance_into_array(json_reader: *std.Io.Reader) !void {
     try advance_past_whitespace(json_reader);
     const byte = json_reader.peek(1) catch |err| switch (err) {
         error.EndOfStream => return error.UnexpectedEndOfStream,
         else => return err,
     };
 
-    if (byte.len > 0 and byte[0] == '[') {
+    if (byte[0] == '[') {
         _ = try json_reader.takeByteSigned();
         return;
     } else {
@@ -33,100 +89,75 @@ fn advance_to_first_pair(json_reader: *std.Io.Reader) !void {
     }
 }
 
+test "advance_into_array when array" {
+    const test_input = "[{";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [4]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try advance_into_array(&limited.interface);
+
+    const next_byte = try limited.interface.peek(1);
+    try std.testing.expectEqual('{', next_byte[0]);
+}
+
+test "advance_into_array when not array" {
+    const test_input = "{";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [4]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try std.testing.expectError(error.ExpectedArrayStart,  advance_into_array(&limited.interface));
+}
+
+fn advance_into_object(json_reader: *std.Io.Reader) !void {
+    try advance_past_whitespace(json_reader);
+
+    const byte = json_reader.peek(1) catch |err| switch (err) {
+        error.EndOfStream => return error.UnexpectedEndOfStream,
+        else => return err,
+    };
+
+    if (byte[0] == '{') {
+        _ = try json_reader.takeByteSigned();
+        return;
+    } else {
+        return error.ExpectedObjectStart;
+    }
+}
+
+test "advance_into_object when object" {
+    const test_input = " {\"";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [4]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try advance_into_object(&limited.interface);
+
+    const next_byte = try limited.interface.peek(1);
+    try std.testing.expectEqual('"', next_byte[0]);
+}
+
+test "advance_into_object when not object" {
+    const test_input = "\"";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [4]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    try std.testing.expectError(error.ExpectedObjectStart,  advance_into_object(&limited.interface));
+}
+
+
 fn read_next_pair(json_reader: *std.Io.Reader) !void {
-    _ = json_reader;
+    try advance_into_object(json_reader);
 }
 
 pub fn calculate(json_reader: *std.Io.Reader) !void {
-    advance_to_first_pair(json_reader);
+    try advance_into_array(json_reader);
     while (read_next_pair(json_reader)) |pair| {
         _ = pair;
         // do work
     } else |err| switch (err) {
         else => return err,
     }
-}
-
-test "advance_past_whitespace with spaces and tabs" {
-    const test_input = "   \t\n\r{";
-
-    const file = try std.fs.cwd().createFile("test_whitespace.tmp", .{ .read = true, .truncate = true });
-    defer file.close();
-    defer std.fs.cwd().deleteFile("test_whitespace.tmp") catch {};
-
-    try file.writeAll(test_input);
-    try file.seekTo(0);
-
-    var buffer: [64]u8 = undefined;
-    var reader_wrapper = file.reader(&buffer);
-
-    try advance_past_whitespace(&reader_wrapper.interface);
-
-    const next_byte = try reader_wrapper.interface.peek(1);
-    try std.testing.expect(next_byte.len > 0);
-    try std.testing.expect(next_byte[0] == '{');
-}
-
-test "advance_past_whitespace with no whitespace" {
-    const test_input = "{\"test\": 123}";
-
-    const file = try std.fs.cwd().createFile("test_no_whitespace.tmp", .{ .read = true, .truncate = true });
-    defer file.close();
-    defer std.fs.cwd().deleteFile("test_no_whitespace.tmp") catch {};
-
-    try file.writeAll(test_input);
-    try file.seekTo(0);
-
-    var buffer: [64]u8 = undefined;
-    var reader_wrapper = file.reader(&buffer);
-
-    try advance_past_whitespace(&reader_wrapper.interface);
-
-    const next_byte = try reader_wrapper.interface.peek(1);
-    try std.testing.expect(next_byte.len > 0);
-    try std.testing.expect(next_byte[0] == '{');
-}
-
-test "advance_past_whitespace with empty stream" {
-    const test_input = "";
-
-    const file = try std.fs.cwd().createFile("test_empty.tmp", .{ .read = true, .truncate = true });
-    defer file.close();
-    defer std.fs.cwd().deleteFile("test_empty.tmp") catch {};
-
-    try file.writeAll(test_input);
-    try file.seekTo(0);
-
-    var buffer: [64]u8 = undefined;
-    var reader_wrapper = file.reader(&buffer);
-
-    try advance_past_whitespace(&reader_wrapper.interface);
-
-    const next_byte = reader_wrapper.interface.peek(1) catch |err| switch (err) {
-        error.EndOfStream => return, // Expected for empty stream
-        else => return err,
-    };
-    try std.testing.expect(next_byte.len == 0);
-}
-
-test "advance_past_whitespace with only whitespace" {
-    const test_input = "   \t\n\r   ";
-
-    const file = try std.fs.cwd().createFile("test_only_whitespace.tmp", .{ .read = true, .truncate = true });
-    defer file.close();
-    defer std.fs.cwd().deleteFile("test_only_whitespace.tmp") catch {};
-
-    try file.writeAll(test_input);
-    try file.seekTo(0);
-
-    var buffer: [64]u8 = undefined;
-    var reader_wrapper = file.reader(&buffer);
-
-    try advance_past_whitespace(&reader_wrapper.interface);
-
-    const next_byte = reader_wrapper.interface.peek(1) catch |err| switch (err) {
-        error.EndOfStream => return, // Expected for empty stream
-        else => return err,
-    };
-    try std.testing.expect(next_byte.len == 0);
 }
