94d6bd574d762b2f9a0657fd620b0d6a5a20d7e3
James Cox-Morton <th3james@hey.com>
2025-11-16 16:18:06 +0000

    Simplify parser, get core outer loop

--- src/calculate.zig
+++ src/calculate.zig
@@ -74,43 +74,7 @@ test "advance_past_whitespace with only whitespace" {
     try std.testing.expectEqual(0, next_byte.len);
 }
 
-fn advance_into_array(json_reader: *std.Io.Reader) !void {
-    try advance_past_whitespace(json_reader);
-    const byte = json_reader.peek(1) catch |err| switch (err) {
-        error.EndOfStream => return error.UnexpectedEndOfStream,
-        else => return err,
-    };
-
-    if (byte[0] == '[') {
-        _ = try json_reader.takeByteSigned();
-        return;
-    } else {
-        return error.ExpectedArrayStart;
-    }
-}
-
-test "advance_into_array when array" {
-    const test_input = "[{";
-    var fixed_reader = std.Io.Reader.fixed(test_input);
-    var buffer: [4]u8 = undefined;
-    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
-
-    try advance_into_array(&limited.interface);
-
-    const next_byte = try limited.interface.peek(1);
-    try std.testing.expectEqual('{', next_byte[0]);
-}
-
-test "advance_into_array when not array" {
-    const test_input = "{";
-    var fixed_reader = std.Io.Reader.fixed(test_input);
-    var buffer: [4]u8 = undefined;
-    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
-
-    try std.testing.expectError(error.ExpectedArrayStart,  advance_into_array(&limited.interface));
-}
-
-fn advance_into_object(json_reader: *std.Io.Reader) !void {
+fn expect_and_consume_char(json_reader: *std.Io.Reader, expected_char: u8) !void {
     try advance_past_whitespace(json_reader);
 
     const byte = json_reader.peek(1) catch |err| switch (err) {
@@ -118,46 +82,83 @@ fn advance_into_object(json_reader: *std.Io.Reader) !void {
         else => return err,
     };
 
-    if (byte[0] == '{') {
+    if (byte[0] == expected_char) {
         _ = try json_reader.takeByteSigned();
         return;
     } else {
-        return error.ExpectedObjectStart;
+        return error.UnexpectedCharacter;
     }
 }
 
-test "advance_into_object when object" {
+test "expect_and_consume_char with valid char advances" {
     const test_input = " {\"";
     var fixed_reader = std.Io.Reader.fixed(test_input);
     var buffer: [4]u8 = undefined;
     var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
 
-    try advance_into_object(&limited.interface);
+    try expect_and_consume_char(&limited.interface, '{');
 
     const next_byte = try limited.interface.peek(1);
     try std.testing.expectEqual('"', next_byte[0]);
 }
 
-test "advance_into_object when not object" {
+test "advance_into_object when not object returns error" {
     const test_input = "\"";
     var fixed_reader = std.Io.Reader.fixed(test_input);
     var buffer: [4]u8 = undefined;
     var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
 
-    try std.testing.expectError(error.ExpectedObjectStart,  advance_into_object(&limited.interface));
+    try std.testing.expectError(error.UnexpectedCharacter, expect_and_consume_char(&limited.interface, 'a'));
+}
+
+const Pair = struct {
+    x0: f64,
+    y0: f64,
+    x1: f64,
+    y1: f64,
+};
+
+fn read_pair(json_reader: *std.Io.Reader) !Pair {
+    const result = Pair{
+        .x0 = 0,
+        .y0 = 0,
+        .x1 = 0,
+        .y1 = 0,
+    };
+    try expect_and_consume_char(json_reader, '{');
+    try expect_and_consume_char(json_reader, '}');
+
+    return result;
 }
 
 
-fn read_next_pair(json_reader: *std.Io.Reader) !void {
-    try advance_into_object(json_reader);
+test "read_pair when valid succeeds" {
+    const test_input = "{ \"x0\": 1.2, \"y0\": 2.3, \"x1\": 3.4, \"y1\": 4.5 }";
+    var fixed_reader = std.Io.Reader.fixed(test_input);
+    var buffer: [4]u8 = undefined;
+    var limited = std.Io.Reader.Limited.init(&fixed_reader, @enumFromInt(test_input.len), &buffer);
+
+    const pair = try read_pair(&limited.interface);
+
+    try std.testing.expectEqual(1.2, pair.x0);
+    try std.testing.expectEqual(2.3, pair.y0);
+    try std.testing.expectEqual(3.4, pair.x1);
+    try std.testing.expectEqual(4.5, pair.y1);
 }
 
 pub fn calculate(json_reader: *std.Io.Reader) !void {
-    try advance_into_array(json_reader);
-    while (read_next_pair(json_reader)) |pair| {
-        _ = pair;
-        // do work
-    } else |err| switch (err) {
-        else => return err,
+    try expect_and_consume_char(json_reader, '[');
+
+    while(true) {
+        try advance_past_whitespace(json_reader);
+        const next_char = try json_reader.peek(1);
+
+        if (next_char == ']') {
+            break;
+        }
+
+        try read_pair(json_reader);
     }
+
+    try expect_and_consume_char(json_reader, ']');
 }
